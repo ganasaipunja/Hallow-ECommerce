7088174c8331b01d5eaa3d1f59939fd441376994
ganasaipunja <ganasaisai624@gmail.com>
2026-03-02 11:06:53 +0530

    Fix: Set DEBUG to False and verified static settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -112,5 +112,8 @@ REST_FRAMEWORK = {
     ],
 }
 
+STATIC_URL = '/static/' # Slash mundu undali
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # WhiteNoise production settings
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
