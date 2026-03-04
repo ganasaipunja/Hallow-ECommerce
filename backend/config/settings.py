8a4bd7f01f93799707952dedba299c4bee62c116
ganasaipunja <ganasaisai624@gmail.com>
2026-03-04 20:23:48 +0530

    Switch to Brevo HTTP API to permanently bypass Render SMTP blocks

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -94,14 +94,14 @@ else:
         }
     }
 
-# Email settings
+# Email settings (No longer used for OTP, overridden by Brevo HTTP API)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 465
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER') 
-EMAIL_HOST_PASSWORD = os.environ.get('BREVO_API_KEY') 
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') 
 DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 AUTH_USER_MODEL = 'api.User'
